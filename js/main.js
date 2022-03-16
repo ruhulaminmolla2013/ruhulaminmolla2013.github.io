ca4c4bab4895ec55a3b0c3bda1a1227cce611b28
ruhul <ruhulaminmolla2013@gmail.com>
2022-03-16 12:03:36 +0530

    Mobile complete

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+const menu = document.getElementById("menu");
+const drawer = document.getElementById("drawer");
+const close = document.getElementById("close-icon");
+
+menu.addEventListener("click", () => {
+  drawer.classList.add("drawer__open");
+  drawer.classList.remove("drawer__close");
+  menu.classList.remove("menu-icon__show");
+  menu.classList.add("menu-icon__hide");
+});
+close.addEventListener("click", () => {
+  drawer.classList.remove("drawer__open");
+  drawer.classList.add("drawer__close");
+  menu.classList.remove("menu-icon__hide");
+  menu.classList.add("menu-icon__show");
+});
