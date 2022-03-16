fe20c4e10087788f778aab61ce102655a93c8276
ruhul <ruhulaminmolla2013@gmail.com>
2022-03-16 18:18:32 +0530

    response working

--- js/main.js
+++ js/main.js
@@ -1,16 +1,38 @@
-const menu = document.getElementById("menu");
+const menuIcon = document.getElementById("menu");
 const drawer = document.getElementById("drawer");
-const close = document.getElementById("close-icon");
+const closeIcon = document.getElementById("close-icon");
+const nav = document.getElementById("nav");
 
-menu.addEventListener("click", () => {
+menuIcon.addEventListener("click", openDrawer);
+closeIcon.addEventListener("click", closeDrawer);
+// nav.addEventListener("click", closeDrawer);
+
+function openDrawer() {
   drawer.classList.add("drawer__open");
   drawer.classList.remove("drawer__close");
-  menu.classList.remove("menu-icon__show");
-  menu.classList.add("menu-icon__hide");
-});
-close.addEventListener("click", () => {
+  menuIcon.classList.remove("menu-icon__show");
+  menuIcon.classList.add("menu-icon__hide");
+}
+
+function closeDrawer() {
   drawer.classList.remove("drawer__open");
   drawer.classList.add("drawer__close");
-  menu.classList.remove("menu-icon__hide");
-  menu.classList.add("menu-icon__show");
-});
+  menuIcon.classList.remove("menu-icon__hide");
+  menuIcon.classList.add("menu-icon__show");
+}
+
+/** active class to nav bar*** */
+// Get the container element
+var btnContainer = document.getElementById("nav");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("navItem");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
